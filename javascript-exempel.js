ca37e0f49273555ab6a497763d9714dd2ad4acd0
EdwarF7 <edwar.fanoun@elev.ga.ntig.se>
2021-03-23 15:38:09 +0100

    Extern JS.fil

--- javascript-exempel.js
+++ javascript-exempel.js
@@ -1,3 +1,20 @@
+
 function visaDialog(){
-    alert('Välkommen!');
+    alert('Sida skapad av Edwar Fanoun 2021 Mars');
+}
+
+function changeTextAndBack(){
+    document.getElementById("head1").innerHTML = "Sida skapad av Edwar Fanoun 2021 Mars";
+    document.getElementById("par1").style.backgroundColor = "lightblue";
+}
+
+function changeImg(){
+    if (document.getElementById("imgClickChange").src == "https://pixy.org/src/132/1320328.jpg")
+    {
+        document.getElementById("imgClickChange").src = "https://pixy.org/src/92/928025.jpg";
+    }
+    else
+    {
+        document.getElementById("imgClickChange").src = "https://pixy.org/src/132/1320328.jpg";
+    }
 }
